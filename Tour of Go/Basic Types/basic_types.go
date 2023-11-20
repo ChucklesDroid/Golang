def88ef04608a82b0d2553c26696202796315cab
Aakarsh MJ <mj.akarsh@gmail.com>
2023-11-21 01:41:32 +0530

    added basic types from tour of go
    Signed-off-by: Aakarsh MJ <mj.akarsh@gmail.com>

---
+++ Tour of Go/Basic Types/basic_types.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	// var boolean bool = false
+	// var a int = -2  // integer 32 bit
+	// var b uint = 2  // unsigned integer 32 bit
+	// var c byte = 8  // 8 bit integer
+	// var d rune = 32 // 32 bit integer
+	// var e *int = &a
+
+	// var f float32 = 3.1
+	// var g float64 = 32.1
+
+	// var h complex64 = 1 + 2i
+
+	// We can also do the following
+	var (
+		boolean bool      = true
+		a       int       = -2
+		b       uint      = 2
+		c       byte      = 8
+		d       rune      = 32
+		e       *int      = &a
+		f       float32   = 3.2
+		g       float64   = 32.2
+		h       complex64 = 1 + 2i
+	)
+
+	fmt.Println(boolean)
+	fmt.Print(a, b, c, d, e, f, g, h, "\n")
+	fmt.Println(reflect.TypeOf(e)) // To print variable type
+}
